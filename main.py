00e15e1ec5420eb8cdb83db8ab15d794858e91e8
CHiekhA <cheikhm@el-technology.com>
2024-02-26 19:54:10 +0000

    h

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ from langchain_community.llms import OpenAI
 from langchain.indexes import VectorstoreIndexCreator
 from langchain.chains import RetrievalQA
 import os
-openai_api_key = os.getenv("openai_api_key")
 
 from dotenv import load_dotenv
 
+
 load_dotenv()
 
-openai_api_key = os.getenv("OPENAI_API_KEY")
-st.title('ChatWithCSV')
+openai_api_key = os.getenv("openai_api_key")
 
+st.title('ChatWithCSV')
 
 def load_and_index_data(file_path):
     loader = CSVLoader(file_path=file_path)
@@ -23,8 +23,10 @@ user_question = st.text_input("Enter your question :")
 if user_question:
     with st.spinner('Processing...'):
         try:
+           
+            llm = OpenAI(api_key=openai_api_key)
             docsearch = load_and_index_data('DS_250K_GM_096C_full.csv')
-            chain = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type="stuff", retriever=docsearch.vectorstore.as_retriever(), input_key="question")
+            chain = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=docsearch.vectorstore.as_retriever(), input_key="question")
             response = chain({"question": user_question})
             st.write("Answer:", response['result'])
         except Exception as e:
